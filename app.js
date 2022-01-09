2ff0b57abdc54f67217165db1987d514d9050873
Pooja Shah <pshah_be19@thapar.edu>
2022-01-09 22:23:23 +0545

    hello

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ const requestedTitle = _.lowerCase(req.params.postName);
 
 
 
-app.listen(3000 || process.env.PORT, function() {
+app.listen(Process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
 
