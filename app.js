56678ce36b6af25a844c12fbe4d476ecaf97e8ac
Pooja Shah <pshah_be19@thapar.edu>
2022-01-26 22:10:52 +0545

    Inital Commit

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
 
-let app = express();
 
+
+let app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -14,23 +15,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
-const items=[];
-
-const homeStartingContent = "Welcome to my Diary ! ";
+const homeStartingContent = "";
 
 const aboutContent = "I am Pooja Shah . Student of computer science and Engineering.I am From Kathmandu, Nepal. I have completed my high School and Primary School in Kathmandu, Nepal from Modern Indian School. Currently i am in 3rd year pursuing my B.tech degree in Computer Science and Engineering. ";
 
 const contactContent = "I'd love if you reached out to me. Even if it's just to say Hey!. You can contact me through any of my socials.";
 
+const mongoose=require('mongoose');
+mongoose.connect("mongodb+srv://admin-puja:test123@cluster0.6qrl2.mongodb.net/blogDB", {useNewUrlParser: true});
+
+const postSchema={
+  title: String,
+  content :String
+};
+
+const Post = mongoose.model("Post",postSchema);
+
+
 
 app.get("/",function(req,res){
-  res.render("home",{
-    ContentHome:homeStartingContent,
-    items:items
+
+  Post.find({},function(err,posts){
+     res.render("home",{
+       startingContent:homeStartingContent,
+        //items:items,
+        posts:posts
+  });
   });
   //console.log(items);
-})
+});
 
 
 app.get("/about",function(req,res){
@@ -50,31 +63,30 @@ app.post("/compose",function(req,res){
  //let type=req.body.PostBody;
  //console.log(type);
 
- const post={
-  title: req.body.TitleBody,
-  content: req.body.PostBody
-  };
+ const post = new Post ({
+  title: req.body.postTitle,
+  content: req.body.postBody
+ });
 
- items.push(post);
- res.redirect("/");
+  post.save(function(err){
+   if (!err){
+       res.redirect("/");
+   }
 
+ });
+ //res.redirect("/");
 });
 
 //DYNAMIC URL
-app.get("/items/:postName",function(req,res){
-const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId",function(req,res){
+  //const requestedTitle = _.lowerCase(req.params.postName);
+  const requestedPostId = req.params.postId;
 
-  items.forEach(function(post){
-  const storeTitle = _.lowerCase(post.title);
-
-   if(requestedTitle === storeTitle )
-   {
-   //console.log("Match Found!");
-   res.render("post",{
-     title:post.title,
-     content:post.content
-   });
-   }
+  Post.findOne({_id:requestedPostId},function(err,post){
+    res.render("post",{
+      title:post.title,
+      content:post.content
+    });
 
     });
 });
@@ -83,7 +95,14 @@ const requestedTitle = _.lowerCase(req.params.postName);
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.post("/delete", function(req, res){
+  const deletePost = req.body.deletePost;
+  Post.findOneAndRemove({title: deletePost}).then(res.redirect("/"));
+});
+
+
+
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
