9dc7e09db0a575db4cf1df852f97c4e8185ea69f
Pooja Shah <pshah_be19@thapar.edu>
2022-01-26 22:19:24 +0545

    Added gitignore

--- app.js
+++ app.js
@@ -91,19 +91,18 @@ app.get("/posts/:postId",function(req,res){
     });
 });
 
-
-
-
-
 app.post("/delete", function(req, res){
   const deletePost = req.body.deletePost;
   Post.findOneAndRemove({title: deletePost}).then(res.redirect("/"));
 });
 
+let port = process.env.PORT;
+ if (port == null || port == "") {
+   port = 3000;
+ }
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started.");
 });
 
 
